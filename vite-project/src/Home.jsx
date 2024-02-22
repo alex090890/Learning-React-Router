a21dacc0849f61e9f04a7eb2595f660cab5ecde2
alexprofteach@gmail.com <alexprofteach@gmail.com>
2024-02-22 01:13:24 +0100

    Final styling

--- vite-project/src/Home.jsx
+++ vite-project/src/Home.jsx
@@ -4,8 +4,8 @@ function Home() {
   return (
      <div>
       <h1>This is the home page</h1>
-      <Link to="about">Click to view our about page</Link>
-      <Link to="contact">Click to view our contact page</Link>
+      <Link to="about" className="link">Click to view our about page</Link>
+      <Link to="contact" className="link">Click to view our contact page</Link>
     </div>
   );
 }
